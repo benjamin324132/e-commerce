6792073441106255af093cc9201b76da8fb620a2
benjamin324132 <landavazobenjamin@gmail.com>
2023-08-04 13:23:54 -0700

    Add changes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,8 +4,8 @@ export default function NotFound() {
   return (
     <div className="min-h-screen w-full grid place-items-center">
       <Heading
-        title="Product not found"
-        subtitle="Please check the url is correct"
+        title="404"
+        subtitle="The page you are looking for no exist"
         center
       />
     </div>
